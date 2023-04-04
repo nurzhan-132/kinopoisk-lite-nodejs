f02de3a812f7e6703b6b7f291f5eae17ce9e5720
Nurzhan Momynkul <nurzhanmomynov01@gmail.com>
2023-04-04 15:59:08 +0600

    initial commit

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+
+const app = express();
+
+app.use(express.static(__dirname + '/public'))
+app.set("view engine", "ejs")
+
+app.get('/', (req, res) => {
+    res.render('index')
+})
+app.get('/login', (req, res) => {
+    res.render('login')
+})
+app.get('/register', (req, res) => {
+    res.render('register')
+})
+
+const PORT = 3000;
+app.listen(PORT, () => {
+    console.log(`Server listening on port ${PORT}`);
+})
